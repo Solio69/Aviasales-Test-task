a8ac341d0a5e39ccf1dfa462e97767a05bd5251c
Anastasiya Kazanina <61517307+Solio69@users.noreply.github.com>
2021-11-22 12:42:54 +0300

    checkbox

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -3,23 +3,43 @@ import styles from './Filters.module.scss'
 const Filters =()=>{
 
     return(
-        <div className={styles.filters}>
+        <div className={styles['filters']}>
             <div>Количество пересадок</div>
             <ul>
                 <li>
-                    <label><input type="checkbox"/>Все</label>
+                    <label className={styles['checkbox-label']}>
+                        <input type="checkbox" className={styles['checkbox-input']}/>
+                        <span className={styles['check-box']}></span>
+                        Все
+                    </label>
                 </li>
                 <li>
-                    <label><input type="checkbox"/>Без пересадок</label>
+                    <label className={styles['checkbox-label']}>
+                        <input type="checkbox" className={styles['checkbox-input']}/>
+                        <span className={styles['check-box']}></span>
+                        Без пересадок
+                    </label>
                 </li>
                 <li>
-                    <label><input type="checkbox"/>1 пересадка</label>
+                    <label className={styles['checkbox-label']}>
+                        <input type="checkbox" className={styles['checkbox-input']}/>
+                        <span className={styles['check-box']}></span>
+                        1 пересадка
+                    </label>
                 </li>
                 <li>
-                    <label><input type="checkbox"/>2 пересадки</label>
+                    <label className={styles['checkbox-label']}>
+                        <input type="checkbox" className={styles['checkbox-input']}/>
+                        <span className={styles['check-box']}></span>
+                        2 пересадки
+                    </label>
                 </li>
                 <li>
-                    <label><input type="checkbox"/>3 пересадки</label>
+                    <label className={styles['checkbox-label']}>
+                        <input type="checkbox" className={styles['checkbox-input']}/>
+                        <span className={styles['check-box']}></span>
+                        3 пересадки
+                    </label>
                 </li>
             </ul>
         </div>
